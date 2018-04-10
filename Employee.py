e44de327dd13c541d17f36d74d0e434722853345
raahool007 <raahool.deshmukh@gmail.com>
2018-04-11 01:10:53 +0530

    Initial commit

---
+++ Employee.py
@@ -0,0 +1,26 @@
+class Employee:
+    def setNumberOfWorkingHours(self):
+        self.numberOfWorkingHours = 40
+
+    def printNumberOfWorkingHours(self):
+        print(self.numberOfWorkingHours)
+
+class Trainee(Employee):
+    def setNumberOfWorkingHours(self):
+        self.numberOfWorkingHours = 45
+
+    def resetNumberOfWorkingHours(self):
+        super().setNumberOfWorkingHours()
+
+employee = Employee()
+employee.setNumberOfWorkingHours()
+print("Number of employee working hours",end=' ')
+employee.printNumberOfWorkingHours()
+
+trainee = Trainee()
+trainee.setNumberOfWorkingHours()
+print("Number of trainee working hours",end=' ')
+trainee.printNumberOfWorkingHours()
+trainee.resetNumberOfWorkingHours()
+print("Number of trainee working hours after call to super ",end=' ')
+trainee.printNumberOfWorkingHours()
